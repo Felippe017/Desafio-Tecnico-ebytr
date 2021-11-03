b631a0b73c308f31edd97e4156daced2522db77f
Felippe017 <felippecorrea2010@gmail.com>
2021-11-03 17:14:47 -0300

    Criando arquitetura MSC: criando pasta routers dentro da pasta src

---
+++ backend/src/routers/toDoRoute.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.post('/');
+
+router.get('/');
+
+router.put('/:id');
+
+router.delete('/:id');
+
+module.exports = router;
