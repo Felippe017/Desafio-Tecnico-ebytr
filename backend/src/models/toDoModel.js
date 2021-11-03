c9cb766a3b453ebef0afc2a1ae58059af4b1baf8
Felippe017 <felippecorrea2010@gmail.com>
2021-11-03 17:56:00 -0300

    Criando arquitetura MSC: criando funçao updateTask  no models

--- backend/src/models/toDoModel.js
+++ backend/src/models/toDoModel.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require('mongodb');
 const { connection } = require('./connection');
 
 const getAll = async () => {
@@ -7,14 +8,39 @@ const getAll = async () => {
   return tasks;
 };
 
-const createTasks = async ({ task }) => {
+const createTasks = async ({ task, statusTask }) => {
   const tasksCollection = await connection();
   const dbTasks = await tasksCollection.collection('toDo');
-  const createTask = await dbTasks.insertOne({ task });
+  const createTask = await dbTasks.insertOne({ task, statusTask });
   return createTask;
+};
+
+const getAllByid = async ({ id }) => {
+  if (!ObjectId.isValid(id)) {
+    return null;
+  }
+  const tasksCollection = await connection();
+  const dbTasks = await tasksCollection.collection('toDo');
+  const tasks = await dbTasks.findOne({ _id: ObjectId(id) });
+  return tasks;
+};
+
+const updateTask = async ({ task, statusTask }, id) => {
+  if (!ObjectId.isValid(id)) return null;
+
+  const tasksCollection = await connection();
+  const dbTasks = tasksCollection.collection('toDo');
+  const tasksUpdate = dbTasks.updateOne({ _id: ObjectId(id) },
+    {
+      $set: { task, statusTask },
+    });
+
+  return tasksUpdate;
 };
 
 module.exports = {
   getAll,
   createTasks,
+  getAllByid,
+  updateTask,
 };
